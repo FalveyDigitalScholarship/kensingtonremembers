fedae2502ef8e13237200ea407d77e1372f31006
Erica Y. Hayes <ericay.hayes@gmail.com>
2020-07-24 12:21:20 -0400

    first commit--Gordon's Kensington Remembers Map

---
+++ google-doc-url.js
@@ -0,0 +1,1 @@
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GMfA8fzfSlDPET8O1_6uWvBOjC4M5-8eRiy4UlJRtrc/';
---
+++ scripts/constants.js
@@ -0,0 +1,24 @@
+var constants = {
+	// Storymapap Info
+  _mapTitle: 'Storymap Title',
+  _mapSubtitle: 'Storymap Subtitle',
+  _mapLogo: 'Storymap Logo',
+  // Credits
+	_authorName: 'Author Name',
+  _webDeveloper: 'Web Development Assistant',
+  _projectWebsite: 'Project website',
+	_authorURL: 'Author Email or Website',
+  _webDeveloperURL: 'Web Development Assistant Email',
+  _codeCredit: 'Code Credit',
+	_githubRepo: 'GitHub Repo',
+	// Map Settings
+	_tileProvider: 'Basemap Tiles',
+	_zoomControls: 'Zoom Controls',
+  _narrativeWidth: 'Narrative Width',
+  _imgContainerHeight: 'Media Container Height',
+  _pixelsAfterFinalChapter: 'Pixels After Final Chapter',
+  _narrativeBackground: 'Narrative Background Color',
+  _narrativeText: 'Narrative Text Color',
+  _narrativeActive: 'Active Chapter Background Color',
+  _narrativeLink: 'Narrative Link Color',
+};
