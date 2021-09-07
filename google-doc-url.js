5530524c1326ff7a29aafca6acedd09fd470b7f4
Erica Y. Hayes <ericay.hayes@gmail.com>
2021-09-07 09:54:38 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -3,4 +3,3 @@ var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GMfA8fzfSlDPET8O1_6u
 
 // insert your own Google Sheets API key from https://console.developers.google.com
  var googleApiKey = 'AIzaSyCT5IglO7R3rd9p2VAITZcX_t4M7C8HiZs';
-test
